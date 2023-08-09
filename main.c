bc8a52a274f8715974a743d53b5f0ae305df0f89
Alex Hawking <alexhawking23@gmail.com>
2023-08-09 17:28:41 +0800

    add template file

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * ANSWERS FOR WEEK _ QUESTION _ 
+ *
+ * Who did what (add a line explaining what you did and who you are)
+ * 
+ * - 
+ *
+ *
+ * Add code below this line
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
