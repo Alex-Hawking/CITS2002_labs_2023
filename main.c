fb5af846a294054e572ebf57cfb31353606c3ac8
Isaac Watt <techotroll@DESKTOP-BOGJUTO>
2023-08-09 19:51:25 +0800

    initial commit

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 /*
- * ANSWERS FOR WEEK 1 QUESTION _ 
+ * ANSWERS FOR WEEK 2 QUESTION 1 
  *
  * Who did what (add a line explaining what you did and who you are)
  * 
- * - Alex Hawking: used a greedy algorithm to get minimum change possible
+ * - Isaac Watt: simple answer using if/else
  *
  *
  * Add code below this line
@@ -12,27 +12,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-
 int main(int argc, char *argv[]) {
-    // Get cents as whole assuming product price is represented as decimal
-    int product_price = atof(argv[1]) * 100;
-    int denominations[] = {50, 20, 10, 5, 2, 1}; 
-
-    int num_coins = 6;
-
-    for (int i = 0; i < num_coins; i++) {
-
-        int amount = 0;
-        int current_coin = denominations[i];
-
-        while (product_price - current_coin >= 0) {
-            product_price -= current_coin;
-            amount++;
-        }
-        printf("%dc : %d\n", current_coin, amount); 
+    if(argc != 2){
+        exit(EXIT_FAILURE);
     }
-
+    int year = atoi(argv[1]);
+    if((year % 400 == 0) || (((year % 4 == 0) && (year % 100 != 0)))){
+        printf("%d IS a leap year!\n", year);
+    }
+    else{
+        printf("%d IS NOT a leap year\n", year);
+    }
     return 0;
 }
