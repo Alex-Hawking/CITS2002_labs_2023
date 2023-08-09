c815bc8c07a3b7ae2d01538523e71c61f6328d3e
Alex Hawking <alexhawking23@gmail.com>
2023-08-09 17:46:47 +0800

    add solution

--- main.c
+++ main.c
@@ -12,8 +12,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
-    //Code here
+    // Get cents as whole assuming product price is represented as decimal
+    int product_price = atof(argv[1]) * 100;
+    int denominations[] = {50, 20, 10, 5, 2, 1}; 
+
+    int num_coins = 6;
+
+    for (int i = 0; i < num_coins; i++) {
+
+        int amount = 0;
+        int current_coin = denominations[i];
+
+        while (product_price - current_coin >= 0) {
+            product_price -= current_coin;
+            amount++;
+        }
+        printf("%dc : %d\n", current_coin, amount); 
+    }
 
     return 0;
 }
