ff3e49554f10e1872a344e0f71ec945bcd4be60a
Alex Hawking <alexhawking23@gmail.com>
2023-08-14 16:56:08 +0800

    string len function

--- main.c
+++ main.c
@@ -12,8 +12,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int my_strlen(char string[]) {
+    int i = 0;
+    while (string[i]) {
+        i++;
+    }
+    return i;
+}
+
 int main(int argc, char *argv[]) {
     //Code here
+
+    printf("%i\n", my_strlen(argv[1]));
 
     return 0;
 }
